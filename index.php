94e1537c0f17215cbf8346b6853171621c21ba45
DyCyD <55021240+DyCyD@users.noreply.github.com>
2021-08-26 23:19:59 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$rand = rand(0, 50); ?>
+<!DOCTYPE html>
+<html lang="hu" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php
+    if ($rand == 50) {
+      echo "ǝɹǝɥʇollǝɥ";
+    }
+    else {
+      echo "‎";
+    } ?></title>
+  </head>
+  <body>
+  </body>
+</html>
